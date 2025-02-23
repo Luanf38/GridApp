f7d44bf932db427e189109bcb0be6571a7ba9d77
Luanf38 <luanfelipe38@hotmail.com>
2025-02-23 12:57:41 -0300

    Gird feito

--- app/src/main/java/com/luanlima/gridapp/MainActivity.kt
+++ app/src/main/java/com/luanlima/gridapp/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.luanlima.gridapp
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Star
@@ -27,11 +29,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.luanlima.gridapp.ui.theme.GridAppTheme
-import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +40,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             GridAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Card(
-                        topic = DataSource.topics[0],
+                    GridCard(
                         modifier = Modifier.padding(innerPadding)
+                            .padding(8.dp),
+                        topics = DataSource.topics
                     )
                 }
             }
@@ -57,7 +58,7 @@ fun Card(topic: Topic, modifier: Modifier = Modifier) {
             .height(68.dp)
             .width(180.dp)
             .clip(RoundedCornerShape(10.dp)),
-        color = Color.Gray,
+        color = Color.LightGray,
     ) {
         Row {
             Image(
@@ -100,14 +101,33 @@ fun Card(topic: Topic, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GridCard(
+    modifier: Modifier,
+    topics: List<Topic>
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Adaptive(minSize = 180.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(topics.size) {
+            index -> Card(
+                topic = topics[index]
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GridAppTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Card(
-                topic = DataSource.topics[0],
-                modifier = Modifier.padding(innerPadding)
+            GridCard(
+                modifier = Modifier.padding(innerPadding).padding(8.dp),
+                topics = DataSource.topics
             )
         }
     }
